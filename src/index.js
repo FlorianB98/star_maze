0cadff044c4e35e881d35aa347e21e8b2b31293e
FlorianB98 <florianbrunet88@gmail.com>
2019-01-16 21:57:22 +0100

    add camera gestion and placement

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './css/style.styl'
 import './js/threegest.js'
 
-var generator = require('generate-maze');
-var maze = generator(3);
-console.log(maze)
+let generator = require('generate-maze')
+let maze = generator(3)
--- src/js/threegest.js
+++ src/js/threegest.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 
-let n = 9
+let n = 16
+let touch = false
 let planetTab= []
 import Planet from './planet/planetClass'
 
@@ -59,7 +60,7 @@ const scene = new THREE.Scene()
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 100
+camera.position.z = 400
 scene.add(camera)
 
 /**
@@ -72,15 +73,27 @@ scene.add(space)
 
 for(let i = 0; i < n; i++)
 {
-    const radius = Math.floor(Math.random() * (10 - 10 + 1)) + 10;
+    const radius = Math.floor(Math.random() * (15 - 15 + 1)) + 15;
+
+    let material
+
+    if(i===0){
+        material = new THREE.MeshBasicMaterial({color: 0x008000 })
+    }
+    else if (i === n-1)
+    {
+        material = new THREE.MeshBasicMaterial({color: 0xf00000 })
+    }
+    else{
+        material = new THREE.MeshBasicMaterial({color: 0xffffff })
+    }
 
     const star = new THREE.Mesh(
-        new THREE.SphereGeometry(radius),
-        new THREE.MeshBasicMaterial({color: 0xffffff })
+        new THREE.SphereGeometry(radius,26,26),material
     )
-    star.position.x = ((Math.random() - 0.5) * 100)
-    star.position.z = (Math.random() - 0.5) * 100
-    star.position.y = (Math.random() - 0.5) * 100
+    star.position.x = ((Math.random() - 0.5) * 300)
+    star.position.z = (Math.random() - 0.5) * 300
+    star.position.y = (Math.random() - 0.5) * 300
 
     let planet = new Planet
     planet.x = star.position.x
@@ -88,21 +101,37 @@ for(let i = 0; i < n; i++)
     planet.y = star.position.y
     planet.r = radius
 
+
     planetTab.push(planet)
 
     let test = false
+    if (planetTab.length>1)
+    {
+        while (!test){
+            for(let j = 0; j < planetTab.length-1; j++)
+            {
+                if( 
+                    Math.sqrt( 
+                        ((planet.y - planetTab[j].y)*(planet.y - planetTab[j].y) + 
+                        (planet.x - planetTab[j].x)*(planet.x - planetTab[j].x) + 
+                        (planet.z - planetTab[j].z)*(planet.z - planetTab[j].z)) ) 
+                        <= radius*5){
+                  
+                        planet.x = star.position.x = ((Math.random() - 0.5) * 250)
+                        planet.z = star.position.z = ((Math.random() - 0.5) * 250)
+                        planet.y = star.position.y = ((Math.random() - 0.5) * 250)
+                        j=0
+                }
+            }
+            test = true
+        }
+    }
+
+    planetTab[planetTab.length-1] = planet
 
     
-    // while (!test){
-    //     for(let j = 0; j < planetTab.length-1; i++)
-    //     {
-    //         if()
-    //     }
-    // }
-
     space.add(star)
 }
-console.log(planetTab)
 
 
 
@@ -118,6 +147,38 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize(sizes.width, sizes.height)
 document.body.appendChild(renderer.domElement)
 
+/** 
+ * camera gestion 
+ */
+window.addEventListener('mousedown', function (event) {
+
+    touch = true
+
+    event.preventDefault(); 
+    let mouseX= event.clientX
+    let mouseY = event.clientY
+    window.addEventListener('mousemove', onMouseMove);
+    window.addEventListener('mouseup', onMouseUp);
+
+    function onMouseMove(_event) {
+
+        space.rotation.x += (-(mouseY - _event.clientY) *0.01)
+        space.rotation.y += ( (mouseX - _event.clientX) * 0.01 )
+        camera.lookAt(new THREE.Vector3())
+
+        mouseX = _event.clientX
+        mouseY = _event.clientY
+        console.log(mouseX)
+    }
+
+    function onMouseUp() {
+
+        window.removeEventListener('mouseup', onMouseUp);
+        window.removeEventListener('mousemove', onMouseMove);
+    }
+
+    });
+
 /**
  * Loop
  */
@@ -126,13 +187,13 @@ const loop = () =>
     window.requestAnimationFrame(loop)
 
     // Update house
-    space.rotation.y += 0.003
+    if (!touch){
+        space.rotation.y += 0.003
+    }
 
     // Update camera
-    camera.position.x = cursor.x * 3
-    camera.position.y = - cursor.y * 3
-    camera.lookAt(new THREE.Vector3())
-
+    
+    
     // Renderer
     renderer.render(scene, camera)
 }
